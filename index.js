7c39cb15387d7d03811a196ddbb10a764b359cc9
meryldakin <meryl.k.dakin@gmail.com>
2017-10-16 15:10:08 -0400

    altered Trip to set driverId and passengerID to the id's of the driver and passenger passed in instead of the global driverId and passengerId; changed the tests to reflect this change. also deleted unnecessary arguments of zipcode and starting/ending point in several methods.

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
 // driver has many trips
 // passenger has many trips
 // trips
-  // passenger
-  // driver
-  // belongs to scooter
+//// passenger
+//// driver
 
-let store = {drivers: [], passengers: [], trips: []}
+let store = { drivers: [], passengers: [], trips: [] };
 
-let driverId = 0
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
 class Driver {
-  constructor(name, zipcode){
-    this.name = name
-    this.id = ++driverId
-    store.drivers.push(this)
+  constructor(name) {
+    this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
   }
-  trips(){
-    return store.trips.filter((trip)=> {
-      return trip.driverId == this.id
-    })
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
   }
-  passengers(){
-    return this.trips().map((trip)=> {
-      return trip.passenger()
-    })
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
   }
 }
-
-let passengerId = 0
 
 class Passenger {
-  constructor(name){
-    this.name = name
-    this.id = ++passengerId
-    store.passengers.push(this)
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
   }
 
-  trips(){
-    return store.trips.filter((trip)=> {
-      return trip.passengerId == this.id
-    })
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
   }
-  drivers(){
-    return this.trips().map((trip)=> {
-      return trip.driver()
-    })
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
   }
 }
 
-let tripId = 0
 class Trip {
-  constructor(driver, passenger, startingZip, endingZip){
-    this.driverId = driverId;
-    this.passengerId = passengerId;
-    this.id = ++tripId
-    store.trips.push(this)
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
   }
-  driver(){
-    return store.drivers.find((driver) => { return driver.id === this.driverId })
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
   }
-  passenger(){
-    return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
   }
 }
